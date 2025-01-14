8b4796e20dd3b851052ad19850e71fcc31ff4df0
aditya karhale <adityakarhale7@gmail.com>
2025-01-14 22:10:57 +0530

    imported connector and devlopment of frame of the project

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # cafe
 this is my first project based on python and my sql
 ## functions
+ to add order
+ to view menu
+ to manage employee database
+ exit
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("its cafe")
+#this is to import msql connector
+import mysql.connector
+import datetime
+
+#this is the intro pageish thing for cafe
+print("              Cafe Management System                 ")
+print("--------------------wellcome-------------------------")
+print(datetime.datetime.now())
+print("""1.to make an order
+      2.to view menu
+      3.staff management
+      4.exit""")
+flag = 1
+while flag ==1:
+    inp=int(input("enter your command code: "))
+    if inp == 1:
+        print("make an  order")
+    elif inp == 2:
+        print("        menu        ")
+    elif inp == 3:
+        print("staff management")
+        print("1.view staff list")
+        print("2.search for an employee")
+        print("3.add new employee")
+        print("4.remove an employee")
+    elif inp ==4:
+        print("exit")
+        flag = 0
+        
